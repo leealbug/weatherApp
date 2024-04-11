3376d989d2b8c91e65ea1f4a417713e6b98cd136
Lea <leaalbaugh@yahoo.com>
2024-04-11 13:19:52 -0700

    changed help message

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        message: 'Can\'t help you',
+        message: 'Can\'t help you.',
         name: 'Lea Albaugh'
     })
 })
